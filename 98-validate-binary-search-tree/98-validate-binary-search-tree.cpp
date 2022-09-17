590ff8f79581a79dbf65bf266179a96db560fecb
MayankRajAnand <74301715+MayankRajAnand@users.noreply.github.com>
2022-09-17 13:57:00 +0530

    Time: 15 ms (77.90%), Space: 21.7 MB (30.63%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -9,7 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-#include<bits/stdc++.h>
+
 class Solution {
 public:
     bool isValid(TreeNode* root,long mini,long maxi)
