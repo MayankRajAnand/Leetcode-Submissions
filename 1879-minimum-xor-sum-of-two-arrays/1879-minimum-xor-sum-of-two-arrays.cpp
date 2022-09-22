3ce357c32baf1477af010f9eee97e92e12bf5b2f
MayankRajAnand <74301715+MayankRajAnand@users.noreply.github.com>
2022-09-22 23:47:56 +0530

    Time: 195 ms (10.48%), Space: 44.5 MB (6.92%) - LeetHub

--- 1879-minimum-xor-sum-of-two-arrays/1879-minimum-xor-sum-of-two-arrays.cpp
+++ 1879-minimum-xor-sum-of-two-arrays/1879-minimum-xor-sum-of-two-arrays.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-int tsp(int pos, int mask, int n, vector<vector<int>>&dp, vector<int>&nums1,vector<int>&nums2)
+int tsp(int mask, int pos, int n, vector<vector<int>>&dp, vector<int>&nums1,vector<int>&nums2)
 {
     if(pos==n)
         return 0;
-    if(dp[pos][mask]!=-1)
-        return dp[pos][mask];
+    if(dp[mask][pos]!=-1)
+        return dp[mask][pos];
     int ans=INT_MAX;
     for(int k= 0; k<n; k++)
     {
         if((mask & (1<<k))==0)
         {
-           ans=min(ans,(nums1[pos]^nums2[k]) + tsp(pos+1, mask | (1<<k), n, dp, nums1,nums2));
+           ans=min(ans,(nums1[pos]^nums2[k]) + tsp(mask | (1<<k), pos+1, n, dp, nums1,nums2));
                         
         }
     }
     
-    return dp[pos][mask]=ans;
+    return dp[mask][pos]=ans;
 }
     int minimumXORSum(vector<int>& nums1, vector<int>& nums2) {
         int n=nums1.size();
-        vector<vector<int>>dp(n+1,vector<int>(1<<(n+1),-1));
+        vector<vector<int>>dp(1<<(n+1),vector<int>(n+1,-1));
         return tsp(0,0,n,dp,nums1,nums2);
     }
 };
