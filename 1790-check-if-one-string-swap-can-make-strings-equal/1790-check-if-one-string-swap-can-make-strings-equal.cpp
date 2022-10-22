c8f61536c22137cfb813b0969e99b6344efb728f
MayankRajAnand <74301715+MayankRajAnand@users.noreply.github.com>
2022-10-22 10:51:01 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (23.07%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -3,17 +3,20 @@ public:
     bool areAlmostEqual(string s1, string s2) {
         if(s1.size()!=s2.size())
             return false;
+        vector<int>v;
         int i=0,c=0;
         while(i<s1.size())
         {
             if(s1[i]!=s2[i])
+            {
                 c++;
+                v.push_back(i);                 
+            }
             i++;
         }
-        sort(s1.begin(),s1.end());
-        sort(s2.begin(),s2.end());
-        if(s1==s2 and c<=2)
+        if(c==0  or( c==2 and s1[v[0]]==s2[v[1]]  and s2[v[0]]==s1[v[1]]))
             return true;
         return false;
+        
     }
 };
