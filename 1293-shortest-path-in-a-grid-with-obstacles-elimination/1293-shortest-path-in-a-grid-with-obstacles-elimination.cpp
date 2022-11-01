dfb1cbae84db1ea34574fa03494b3a69ee8a97c4
MayankRajAnand <74301715+MayankRajAnand@users.noreply.github.com>
2022-11-01 19:17:48 +0530

    Time: 986 ms (7.46%), Space: 24.9 MB (61.13%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
     
-    vector<vector<vector<int>>> dp;
-    int s(vector<vector<int>> &grid, int k, int i, int j)
+    int helper(int i,int j,vector<vector<int>>& grid, int k,vector<vector<vector<int>>>&dp)
     {
-        if (i < 0 || j < 0 || j >= grid[0].size() || i >= grid.size() || grid[i][j] == 7) return 1e6; 
-
-        if (grid[i][j] == 1 && k == 0) return dp[i][j][k] = 1e6; 
-
-        if (i == 0 && j == 0) return 0; 
-
-        if (dp[i][j][k] != -1) return dp[i][j][k]; 
-
-        int lastItem = grid[i][j];
-
-        grid[i][j] = 7;   // using as visited position
-
-        if (lastItem == 1) k--;
-
-        int l = s(grid, k, i, j - 1);
-        int r = s(grid, k, i, j + 1);
-        int u = s(grid, k, i + 1, j);
-        int d = s(grid, k, i - 1, j);
-
-        dp[i][j][k] = 1 + min(min(l, r), min(u, d)); 
-        grid[i][j] = lastItem;
-
+        if(i<0 or j<0 or i>=grid.size() or j>=grid[0].size() or grid[i][j]==9)return 1e7;
+        if(grid[i][j]==1 and k==0)return dp[i][j][k]=1e7;
+        if(i==0 and j==0 and k>=0)return 0;
+        if(dp[i][j][k]!=-1)
+            return dp[i][j][k];
+        int temp=grid[i][j];
+        grid[i][j]=9;
+        if(temp==1)k--;
+        int l = 1+helper(i, j - 1,grid, k,dp);
+        int r = 1+helper(i, j + 1,grid, k,dp);
+        int u = 1+helper(i + 1, j,grid, k,dp);
+        int d = 1+helper(i - 1, j,grid, k,dp);
+        dp[i][j][k]=min(min(l,r),min(u,d));
+        grid[i][j] = temp;
         return dp[i][j][k];
+       
+        
+        
     }
-
-    int shortestPath(vector<vector<int>> &grid, int k)
-    {
-        dp = vector<vector<vector<int>>>(grid.size(), vector<vector<int>>(grid[0].size(), vector<int>(k + 1, -1))); 
-        int ans = s(grid, k, grid.size() - 1, grid[0].size() - 1);
-        if (ans >= 1e6) return -1; 
+    int shortestPath(vector<vector<int>>& grid, int k) {
+        vector<vector<vector<int>>> dp(vector<vector<vector<int>>>(grid.size(), vector<vector<int>>(grid[0].size(), vector<int>(k + 1, -1)))); 
+        int ans = helper( grid.size() - 1, grid[0].size() - 1,grid,k,dp);
+        if (ans >= 1e7) return -1; 
         return ans;
     }
-
 };
