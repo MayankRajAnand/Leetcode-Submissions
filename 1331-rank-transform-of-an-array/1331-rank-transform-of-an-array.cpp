3bb147fb6f435e9ce74100ef63ae7cf5f8a316c7
MayankRajAnand <74301715+MayankRajAnand@users.noreply.github.com>
2022-11-06 22:34:26 +0530

    Time: 296 ms (18.46%), Space: 47.6 MB (13.74%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -1,38 +1,23 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-       int n=arr.size();
-    vector<int>ans;
-    if(n==0)return ans;
-    if(n==1)
-    {
-        ans.push_back(1);
-        return ans;
-    }
-    vector<int>temp=arr;
-    sort(temp.begin(),temp.end());
-    map<int,int>m;
-    m[temp[0]]=1;
-    int j=2;
-    for(int i=1;i<temp.size()-1;i++)
-    {
-        if(m.find(temp[i])==m.end() and temp[i]!=temp[i+1])
+        vector<int>ans;
+        set<int>s;
+        for(auto it:arr)
         {
-            m[temp[i]]=j;
-            j++;
+            s.insert(it);
         }
-    }
-    if(m.find(temp[n-1])==m.end())
-        m[temp[n-1]]=j;
-    for(auto it:m)
-    {
-        cout<<it.first<<" "<<it.second<<endl;
-    }
-    
-    for(auto it:arr)
-    {
-        ans.push_back(m[it]);
-    }
-    return ans;
+        int i=1;
+        unordered_map<int,int>m;
+        for(auto it:s)
+        {
+            m[it]=i;
+            i++;
+        }
+        for(auto it:arr)
+        {
+            ans.push_back(m[it]);
+        }
+        return ans;
     }
 };
