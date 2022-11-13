169422bc37ac7f4bf15ec44f40cbb7c8c787cc4e
MayankRajAnand <74301715+MayankRajAnand@users.noreply.github.com>
2022-11-13 15:42:49 +0530

    Time: 6 ms (30.44%), Space: 5.7 MB (99.45%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -3,17 +3,15 @@ public:
     bool isHappy(int n) {
         if(n==1 or n==7)
             return true;
-        if(n<10)
-            return false;
-        long long int sum=0;
+        if(n<10)return false;
+        long long sum=0;
         while(n)
         {
-            int temp=(n%10)*(n%10);
-            sum+=temp;
+            int temp=n%10;
+            sum+=(temp*temp);
             n/=10;
         }
-        if(sum==1)
-            return true;
+        if(sum==1)return true;
         return isHappy(sum);
     }
 };
