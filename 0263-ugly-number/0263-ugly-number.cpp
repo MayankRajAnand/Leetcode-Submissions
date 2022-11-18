e3f782799c991250f78761d8b6894900bf7acdca
MayankRajAnand <74301715+MayankRajAnand@users.noreply.github.com>
2022-11-18 19:26:23 +0530

    Time: 5 ms (31.29%), Space: 6 MB (19.97%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n<=0)
+            return false;
+        vector<int>v{2,3,5};
+        for(int i=0;i<3;i++)
+        {
+            while(n%v[i]==0)
+                n=n/v[i];
+        }
+        if(n==1)
+            return true;
+        return false;
+    }
+};
